52596fc7ba1d53e066335b21be134b6cd5d842bd
dedsylva <dedsylva@gmail.com>
2022-03-14 11:15:52 -0300

    Algs p2 done!

--- src/Algorithms/p2/ans2.py
+++ src/Algorithms/p2/ans2.py
@@ -29,6 +29,60 @@ def n_swaps(cnot):
     """
 
     # QHACK #
+    w0, w1 = cnot.wires.toarray()
+
+    # Checks if a is neighbour of b
+    def check(a,b):
+      if a in graph[b] or a == b:
+        return True
+
+    # Goes until we:
+    #  - Find the target
+    # - Goes back (cyclic) in to other neighbour
+    # - Reach a dead end (no more neighbours)
+    def breadth_search(a, b, neighbours):
+      # We already looked here! (cyclic graph)
+      if len(neighbours) == 0:
+        return 0
+
+      # Spreads and check each neighbour
+      for n in neighbours:
+        res = 0
+
+        # Found the target !
+        if check(w1,n):
+          res += 1
+          return res
+        # We circled back to an existent neighbour
+        elif n in looked: 
+          return 0
+
+        # We keep moving !
+        else:
+          res += 1
+          looked.append(n)
+          neigh_of_n = graph[n]
+
+          # Neighbours we didn't check
+          for l in looked:
+            if l in neigh_of_n:
+              neigh_of_n.remove(l)
+
+          # We go again
+          res += breadth_search(n, b, neigh_of_n)
+
+      return res
+
+
+    # They are neighbours
+    if check(w0, w1):
+      return 0
+
+    looked = [w0]
+    n_swaps = breadth_search(w0, w1, graph[w0])
+
+    # Multiply by 2 for swapping everything back
+    return 2*n_swaps 
 
     # QHACK #
 
